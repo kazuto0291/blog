711cfd7ee6558082b2f15324b5678decc56986a9
kazuto0291 <zutozuto03170291@gmail.com>
2020-02-27 14:27:24 +0900

    model-setting

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,7 @@
+class PostsController < ApplicationController
+
+  def index
+    @post = "コントローラーで定義したインスタンス変数"
+  end
+
+end
---
+++ db/migrate/20200227043527_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.text :title, null: false
+      t.text :text, null: false
+      t.image :string
+      t.refer
+      t.timestamps
+    end
+  end
+end
